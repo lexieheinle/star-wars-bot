528d5a5214e7263209835c60c0b7d895a6ddd74c
lexieheinle <lexie.heinle@gmail.com>
2016-10-17 20:57:00 -0400

    Not working reply

--- bot.js
+++ bot.js
@@ -1,36 +1,18 @@
 'use strict';
+
 const botBuilder = require('claudia-bot-builder');
 const fbTemplate = botBuilder.fbTemplate;
-const rp = require('minimal-request-promise');
 
-module.exports = botBuilder((request, originalApiRequest) => {
-  // If request is not postback
-  if (!request.postback)
-    // We'll get some basic info about the user
-    return rp.get(`https://graph.facebook.com/v2.6/${request.sender}?fields=first_name&access_token=${originalApiRequest.env.facebookAccessToken}`)
-      .then(response => {
-        const user = JSON.parse(response.body)
-        // Then let's send two text messages and one generic template with three elements/bubbles
-        return [
-          `Hello, ${user.first_name}. Welcome to a gallaxy far far away! Ready to start your journey to be a jedi?`,
-          'What can I do for you today?',
-          return new fbTemplate.generic()
-            .addBubble(`NASA's Astronomy Picture of the Day`, 'Satellite icon by parkjisun from the Noun Project')
-              .addImage('https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/apod.png')
-              .addButton('Show', 'SHOW_APOD')
-              .addButton('What is APOD?', 'ABOUT_APOD')
-              .addButton('Website', 'http://apod.nasa.gov/apod/')
-            .addBubble(`Photos from NASA's rovers on Mars`, 'Curiosity Rover icon by Oliviu Stoian from the Noun Project')
-              .addImage('https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/mars-rover.png')
-              .addButton('Curiosity', 'CURIOSITY_IMAGES')
-              .addButton('Opportunity', 'OPPORTUNITY_IMAGES')
-              .addButton('Spirit', 'SPIRIT_IMAGES')
-            .addBubble('Help & info', 'Monster icon by Paulo Sá Ferreira from the Noun Project')
-              .addImage('https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/about.png')
-              .addButton('About the bot', 'ABOUT')
-              .addButton('Credits', 'CREDITS')
-              .addButton('Report an issue', 'https://github.com/stojanovic/space-explorer-bot/issues')
-            .get();
-        ];
-      });
-}
+module.exports = botBuilder(request => `Hello from space explorer bot! Your request was: ${request.text}, ${request}`
+  const generic = new fbTemplate.generic();
+
+      return generic
+        .addBubble('Claudia.js', 'Deploy Node.js microservices to AWS easily')
+          .addUrl('https://claudiajs.com')
+          .addImage('https://claudiajs.com/assets/claudiajs.png')
+          .addButton('Say hello', 'HELLO')
+          .addButton('Go to Github', 'https://github.com/claudiajs/claudia')
+        .addBubble('Claudia Bot Builder')
+          .addImage('https://claudiajs.com/assets/claudia-bot-builder-video.jpg')
+          .addButton('Go to Github', 'https://github.com/claudiajs/claudia-bot-builder')
+        generic.get(););
